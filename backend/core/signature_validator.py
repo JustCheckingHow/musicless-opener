08734defb2b59c864141e4799576b31c9dc47880
JanuszSamborski <janusz.joe.samborski@gmail.com>
2021-12-11 15:21:27 +0100

    feat: request_validation module (response data has to be parsed yet)

---
+++ backend/core/signature_validator.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+
+API_URL = "https://ec.europa.eu/cefdigital/DSS/webapp-demo/validation"
+
+
+def request_validation(file):
+
+    get_resp = requests.get(
+        API_URL,
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+            "Cache-Control": "max-age=0"
+        }
+    )
+
+    soup = BeautifulSoup(get_resp.content, 'html.parser')
+
+
+    csrf_tag = soup.find('meta', attrs={'name': '_csrf'}).get('content')
+    csrf = csrf_tag
+
+    # TODO: more filters are available
+    files = {
+        '_csrf': (None, csrf),
+        'signedFile': ('test.xml', file, 'text/xml'),
+        'validationLevel': (None, 'ARCHIVAL_DATA'),
+    }
+
+    post_resp = requests.post(
+        API_URL,
+        cookies=get_resp.cookies,
+        # tag: {filename, fileobj, content_type, headers}
+        files=files,
+        headers= {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1"
+        }
+    )
+
+    return post_resp
+
+
