a3c20b6c5836952db9e2cfe1941022113546d10d
Jan Ignatowicz <jan.ignatowicz2@gmail.com>
2021-12-11 05:10:47 +0100

    drag-drop-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import FileDropField from "./components/FileDropField"
+import './App.css';
+
+import DropFileInput from './components/DropFileInput';
 
 function App() {
+
+    const onFileChange = (files) => {
+        console.log(files);
+    }
+
     return (
-      <div className="App">
-        Hello world!
-        {FileDropField()}
-      </div>
+        <div className="box">
+            <h2 className="header">
+                Opener
+            </h2>
+            <DropFileInput
+                onFileChange={(files) => onFileChange(files)}
+            />
+        </div>
     );
-  }
-  
+}
+
 export default App;
